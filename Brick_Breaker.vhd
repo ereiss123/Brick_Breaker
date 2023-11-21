cc7209a9440617bbbee6d564d1c2b2f4ed47a85a
DRowbo990 <davidrowbotham98@gmail.com>
2023-11-21 14:46:15 -0700

    branch

--- Brick_Breaker.vhd
+++ Brick_Breaker.vhd
@@ -101,79 +101,99 @@ architecture Behavioral of Brick_Breaker is
         );
     end component;
 
-    -- Signal declaration
-    signal rst : STD_LOGIC;
-    signal rst_l : STD_LOGIC;
-    signal R : STD_LOGIC_VECTOR(3 downto 0);
-    signal G : STD_LOGIC_VECTOR(3 downto 0);
-    signal B : STD_LOGIC_VECTOR(3 downto 0);
-    signal request_data : STD_LOGIC;
-    signal current_line : unsigned(9 downto 0);
-    signal data_pos : unsigned(9 downto 0);
-    signal c0_sig : STD_LOGIC;
-    signal locked_sig : STD_LOGIC;
+    component psuedorandom_gen is
+        generic
+            (seed : unsigned(15 downto 0))
+        port
+        (
+            MAX10_CLK1_50 : in STD_LOGIC;
+            rst_l         : in STD_LOGIC
+            gen_button    : in STD_LOGIC;
+            rand          : out unsigned(7 downto 0)
+        )
 
-    signal next_ball : STD_LOGIC;
+        -- Signal declaration
+        signal rst : STD_LOGIC;
+        signal rst_l : STD_LOGIC;
+        signal R : STD_LOGIC_VECTOR(3 downto 0);
+        signal G : STD_LOGIC_VECTOR(3 downto 0);
+        signal B : STD_LOGIC_VECTOR(3 downto 0);
+        signal request_data : STD_LOGIC;
+        signal current_line : unsigned(9 downto 0);
+        signal data_pos : unsigned(9 downto 0);
+        signal c0_sig : STD_LOGIC;
+        signal locked_sig : STD_LOGIC;
+        signal rand : unsigned(7 downto 0)
+        signal next_ball : STD_LOGIC;
 
-    -- Colors                  R    G    B
-    signal white : color := (x"F", x"F", x"F");
-    signal black : color := (x"0", x"0", x"0");
-    signal red : color := (x"C", x"F", x"3");
-    signal yellow : color := (x"B", x"5", x"9");
-begin
-    rst_l <= KEY(0);
-    rst <= not KEY(0) when KEY(0) = '0' else
+        -- Colors                  R    G    B
+        signal white : color := (x"F", x"F", x"F");
+        signal black : color := (x"0", x"0", x"0");
+        signal red : color := (x"C", x"F", x"3");
+        signal yellow : color := (x"B", x"5", x"9");
+    begin
+        rst_l <= KEY(0);
+        rst <= not KEY(0) when KEY(0) = '0' else
         '0';
 
-    VGA_controller_inst : VGA_controller
-    port map
-    (
-        -- Connections go here
-        clk          => c0_sig,
-        rst_l        => rst_l,
-        R            => R,
-        G            => G,
-        B            => B,
-        request_data => request_data,
-        current_line => current_line,
-        data_pos     => data_pos,
-        VGA_B        => VGA_B,
-        VGA_G        => VGA_G,
-        VGA_R        => VGA_R,
-        VGA_HS       => VGA_HS,
-        VGA_VS       => VGA_VS
-    );
-    PLL_inst : VGA_PLL
-    port
-    map(
-    areset => rst,
-    inclk0 => MAX10_CLK1_50,
-    c0 => c0_sig,
-    locked => locked_sig
-    );
+        VGA_controller_inst : VGA_controller
+        port map
+        (
+            -- Connections go here
+            clk          => c0_sig,
+            rst_l        => rst_l,
+            R            => R,
+            G            => G,
+            B            => B,
+            request_data => request_data,
+            current_line => current_line,
+            data_pos     => data_pos,
+            VGA_B        => VGA_B,
+            VGA_G        => VGA_G,
+            VGA_R        => VGA_R,
+            VGA_HS       => VGA_HS,
+            VGA_VS       => VGA_VS
+        );
+        PLL_inst : VGA_PLL
+        port
+        map(
+        areset => rst,
+        inclk0 => MAX10_CLK1_50,
+        c0 => c0_sig,
+        locked => locked_sig
+        );
 
-    db_inst : debouncer
-    port
-    map(
-    clk => c0_sig,
-    rst => rst,
-    button => KEY(1),
-    button_debounced => next_ball
-    );
+        db_inst : debouncer
+        port
+        map(
+        clk => c0_sig,
+        rst => rst,
+        button => KEY(1),
+        button_debounced => next_ball
+        );
 
-    process (c0_sig, rst_l)
-    begin
-        if (not rst_l = '1') then
-            R <= (others => '0');
-            G <= (others => '0');
-            B <= (others => '0');
-        elsif rising_edge(c0_sig) then
-            if request_data = '1' then
-                R <= red(0);
-                G <= red(1);
-                B <= red(2);
+        psuedorandom_gen_inst : psuedorandom_gen
+        port
+        map
+        (
+        MAX10_CLK1_50 => MAX10_CLK1_50,
+        rst_l => rst_l,
+        gen_button => next_ball,
+        rand => rand
+        );
+        process (c0_sig, rst_l)
+        begin
+            if (not rst_l = '1') then
+                R <= (others => '0');
+                G <= (others => '0');
+                B <= (others => '0');
+                elsif rising_edge(c0_sig) then
+                if request_data = '1' then
+                    R <= red(0);
+                    G <= red(1);
+                    B <= red(2);
 
+                end if;
             end if;
-        end if;
-    end process;
+        end process;
 end architecture Behavioral;
