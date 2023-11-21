9f6b383276db9724ed04d8f9d5910f5c67def25a
Eric Reiss <reiss_eric@hotmail.com>
2023-11-20 20:40:21 -0700

    Fix brick offset

--- Brick_Breaker.vhd
+++ Brick_Breaker.vhd
@@ -129,6 +129,8 @@ architecture rtl of Brick_Breaker is
     signal nbrick_x_idx : integer := 0;
     signal brick_y_idx : integer := 0;
     signal nbrick_y_idx : integer := 0;
+    signal line_parity : std_logic := '0';
+    signal nline_parity : std_logic := '0';
     signal full_brick_x : hhalf_brick_corrid := (0,16,32,48,64,80,96,112,128,144,160,
     176,192,208,224,240,256,272,288,304,320,336,352,368,384,400,416,432,448,464,480,496,
     512,528,544,560,576,592,608,624,-1);
@@ -139,8 +141,6 @@ architecture rtl of Brick_Breaker is
     128,136,144,152,160,168,176,184,192,200,208,216,224,232);
     signal brick_tracker :tracker := (
         ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'),
-        ('1','1','1','1','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'),
-        ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'),
         ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'),
         ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'),
         ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'),
@@ -167,7 +167,9 @@ architecture rtl of Brick_Breaker is
         ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'),
         ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'),
         ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'),
-        ('1','1','1','1','1','1','1','1','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1')
+        ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'),
+        ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0'),
+        ('1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1')
         );
 
 
@@ -219,6 +221,7 @@ begin
             paddle_pos <= (0,0);
             brick_x_idx <= 0;
             brick_y_idx <= 0;
+            line_parity <= '0';
         elsif rising_edge(c0_sig) then
             R <= nR;
             G <= nG;
@@ -227,6 +230,7 @@ begin
             brick_y_idx <= nbrick_y_idx;
             paddle_pos <= npaddle_pos;
             ball_pos <= nball_pos;
+            line_parity <= nline_parity;
         end if;
     end process;
 
@@ -247,6 +251,7 @@ begin
                 -- Necessary to prevent latches
                 nbrick_x_idx <= 0;
                 nbrick_y_idx <= 0;
+					 nline_parity <= '0';
             -- Draw paddle
             elsif current_line >= paddle_pos(1) and current_line < (paddle_pos(1)+5)and
             data_pos >= paddle_pos(0) and data_pos < (paddle_pos(0)+40) 
@@ -257,10 +262,12 @@ begin
                 -- Necessary to prevent latches
                 nbrick_x_idx <= 0;
                 nbrick_y_idx <= 0;
+                nline_parity <= '0';
             -- Draw bricks
             elsif current_line >= 0 and current_line < 240 then
-                nbrick_y_idx <= to_integer(shift_right(current_line, 3)); -- divide current line by 8 
-                if current_line(9) = '1' then -- Odd line (half-brick line)
+                nbrick_y_idx <= to_integer(shift_right(current_line, 3)); -- divide current line by 8
+                nline_parity <= to_unsigned(brick_y_idx,32)(0); 
+                if line_parity = '1' then -- Odd line (half-brick line)
                     if data_pos < 8 then
                         nbrick_x_idx <= 0; -- Deal with first half brick
                     else
@@ -275,7 +282,7 @@ begin
                         nR <= white(0);
                         nG <= white(1);
                         nB <= white(2);
-                    elsif current_line(9) = '1' then -- Odd lines (half-brick)
+                    elsif line_parity = '1' then -- Odd lines (half-brick)
                         if (data_pos = half_brick_x(brick_x_idx)+8) and ((brick_x_idx = 0) or (brick_x_idx = 40)) then
                             nR <= white(0); -- draw vertical mortar line for half-brick
                             nG <= white(1);
@@ -311,6 +318,7 @@ begin
                 nB <= black(2);
                 nbrick_x_idx <= 0;
                 nbrick_y_idx <= 0;
+                nline_parity <= '0';
             end if;
         else
             nR <= black(0);
@@ -318,6 +326,7 @@ begin
             nB <= black(2);
             nbrick_x_idx <= 0;
             nbrick_y_idx <= 0;
+            nline_parity <= '0';
         end if;
     end process;
 
